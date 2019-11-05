c4a1d61d9f9ee6426822a63e64d2eeed3cd4701b
Zachary <zachjoliver@gmail.com>
2019-11-05 15:02:10 -0800

    Finish phase 2 into phase 3

--- skeleton/script.js
+++ skeleton/script.js
@@ -19,13 +19,37 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  // const catFormEl = document.getElementById("cat-form");
+  
+  // document.addEventListener('submit', )
+  // favSub.addEventListener("submit", event => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
+    const input = document.getElementsByClassName("favorite-input")[0];
+    const place = input.value;
+    input.value = "";
+    
+    const newLi = document.createElement("li");
+    newLi.textContent = place;
+
+    const ul = document.getElementById('sf-places');
+    ul.appendChild(newLi);
+  }
+  
+  const favSub = document.getElementsByClassName("favorite-submit")[0];
+  favSub.addEventListener("click", handleSubmit);
 
 
   // adding new photos
 
   // --- your code here!
 
+  const puppyForm = (event) => {
+    console.log('click!')
+    const form = document.querySelector('.photo-form-container');
+  }
 
-
+  const showPupForm = document.querySelector('.photo-show-button');
+  showPupForm.addEventListener("click", puppyForm);
 });
